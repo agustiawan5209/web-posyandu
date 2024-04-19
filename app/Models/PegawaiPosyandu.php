351330e32f75fc9a1f482668b3c4cf1deefad5d6
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-04-20 05:10:25 +0800

    update model pegawai posyandu

--- app/Models/PegawaiPosyandu.php
+++ app/Models/PegawaiPosyandu.php
@@ -10,6 +10,13 @@ class PegawaiPosyandu extends Model
     use HasFactory;
 
     protected $table = 'pegawai_posyandus';
+    protected $fillable = [
+        'user_id',
+        'jabatan',
+        'nama',
+        'no_telpon',
+        'alamat',
+    ];
 
     public function user()
     {
