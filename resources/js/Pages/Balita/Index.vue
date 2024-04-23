cb658c99d7d9cc3603e92294cc99a748145ec6c1
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-04-23 18:46:12 +0800

    Update Index Balita

--- resources/js/Pages/Balita/Index.vue
+++ resources/js/Pages/Balita/Index.vue
@@ -36,11 +36,11 @@ const crud = ref({
 
 <template>
 
-    <Head title="Jadwal Imunisasi" />
+    <Head title="Bayi/Balita" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Data Jadwal Imunisasi</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Data Bayi/Balita</h2>
         </template>
 
         <div class="py-4 relative box-content">
